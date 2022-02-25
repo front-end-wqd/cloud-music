e6a20c2146495ff2a21f8f15e156be88b57451a2
wqd182212 <10104911+wqd182212@user.noreply.gitee.com>
2022-02-25 15:00:28 +0800

    基本样式编写字体库引入

--- cloud-music/src/main/Home/index.js
+++ cloud-music/src/main/Home/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import { renderRoutes } from 'react-router-config';
-import '../../fontawesome/fontawesome-free-5.15.4-web/css/all.min.css';
 import './index.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faSearch,
+    faBars,
+} from '@fortawesome/free-solid-svg-icons';
+import { NavLink } from 'react-router-dom';
 
 function Home(props) {
     const { route } = props;
+
     return (
         <div>
-            <i className="fab fa-app-store"></i>
+            <div className='header'>
+                <span>
+                    <FontAwesomeIcon icon={faBars} />
+                </span>
+                <span>云音乐</span>
+                <span>
+                    <FontAwesomeIcon icon={faSearch} />
+                </span>
+            </div>
+            <div className='nav'>
+                <NavLink to='/recommend' activeClassName='active'>推荐</NavLink>
+                <NavLink to='/singer' activeClassName='active'>歌手</NavLink>
+                <NavLink to='/rank' activeClassName='active'>排行榜</NavLink>
+            </div>
             { renderRoutes(route.routes) }
         </div>
     );
